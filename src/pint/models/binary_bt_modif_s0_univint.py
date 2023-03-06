5a7d7e486df3c78fd8647be4b92b0409f17ca99f
Pavel Kůs <121028879+pavelkus@users.noreply.github.com>
2023-03-06 12:06:30 +0100

    Add files via upload

--- src/pint/models/binary_bt_modif_s0_univint.py
+++ src/pint/models/binary_bt_modif_s0_univint.py
@@ -1,33 +1,17 @@
-"""The BT (Blandford & Teukolsky) model."""
+""" The BT (Blandford & Teukolsky) model. """
+""" PK: Modifications to include the spin-0 DM! """
 
 from pint.models.parameter import floatParameter
-from pint.models.pulsar_binary import PulsarBinary
-from pint.models.stand_alone_psr_binaries.BT_model_modif_s0_univint import BTmodel_modif_s0_univint # this has been changed, too
+from pint.models.pulsar_binary import PulsarBinary  # DO I NEED TO MODIFY STH THERE???
+from pint.models.stand_alone_psr_binaries.BT_model_modif_s0_univint import BTmodel_modif_s0_univint # this has been changed, too; notice: "BT_model_modif_s0_univint" vs "BTmodel_modif_s0_univint" ... the difference lies in the placeholder
 from pint.models.timing_model import MissingParameter
 
 # changed the name of the class from BinaryBT
 class BinaryBT_modif_s0_univint(PulsarBinary):
-    """Blandford and Teukolsky binary model.
-
-    This binary model is described in Blandford and Teukolshy 1976. It is
-    a relatively simple parametrized post-Keplerian model that does not
-    support Shapiro delay calculations.
+    """The binary model of Blandford and Teukolsky, which is extended to include the effect of DM on the pulsar period.
 
     The actual calculations for this are done in
-    :class:`pint.models.stand_alone_psr_binaries.BT_model.BTmodel`.
-
-    Parameters supported:
-
-    .. paramtable::
-        :class: pint.models.binary_bt.BinaryBT
-
-    Notes
-    -----
-    Because PINT's binary models all support specification of multiple orbital
-    frequency derivatives FBn, this is capable of behaving like the model called
-    BTX in tempo2. The model called BTX in tempo instead supports multiple
-    (non-interacting) companions, and that is not supported here. Neither can
-    PINT accept "BTX" as an alias for this model.
+    :class:`pint.models.stand_alone_psr_binaries.BT_model_modif_s0_univint.BTmodel_modif_s0_univint`.
 
     See Blandford & Teukolsky 1976, ApJ, 205, 580.
     """
@@ -39,17 +23,18 @@ class BinaryBT_modif_s0_univint(PulsarBinary):
         self.binary_model_name = "BT_modif_s0_univint"
         self.binary_model_class = BTmodel_modif_s0_univint
 
+        # "GAMMA is added, but with zero value and is not fitted - it is not effectively present anywhere"
         self.add_param(
             floatParameter(
-                name="GAMMA",
+                name="GAMMA",   
                 value=0.0,
                 units="second",
                 description="Time dilation & gravitational redshift",
             )
         )
 
-        # this parameter is added
-        # will be found by optimalization
+        # 1st DM parameter is added
+        # will be searched by optimizing
         self.add_param(
             floatParameter(
                 name="ADM1",
@@ -59,8 +44,8 @@ class BinaryBT_modif_s0_univint(PulsarBinary):
             )
         )
 
-        # this parameter is also added
-        # will be found by optimalization
+        # 2nd DM parameter is added
+        # will be searched by optimizing
         self.add_param(
             floatParameter(
                 name="ADM2",
@@ -70,19 +55,20 @@ class BinaryBT_modif_s0_univint(PulsarBinary):
             )
         )
 
-        # mass scale - added
-        # will be fixed - particular value from some interval
+        # 3rd DM parameter is added: DM angular frequency (closely related to the DM scale) 
+        # will be "given" - a particular value from an interval
+        # Note: DM mass scale: [1e-22, 1e-18] eV
         self.add_param(
             floatParameter(
                 name="MDM",
                 value=0.0,
                 units="1/day",
-                description="DM - mass scale",
+                description="DM - angular oscillation frequency",
             )
         )
 
-        # mass scale - added
-        # will be fixed - particular value from some interval
+        # 4th DM parameter is added: DM phase
+        # will be "given" -  a particular value from the interval [0,2pi]
         self.add_param(
             floatParameter(
                 name="BDM",
@@ -112,22 +98,22 @@ class BinaryBT_modif_s0_univint(PulsarBinary):
             self.GAMMA.value = "0"
             self.GAMMA.frozen = True
         
-        # we follow gamma's example and set Adm1 to 0 if the original value is None
+        # we follow gamma's example and set ADM1 to 0 if the original value is None
         if self.ADM1.value is None:
             self.ADM1.value = "0"
             self.ADM1.frozen = True
         
-        # here is the similar situation for the Adm2
+        # here is the similar situation for the ADM2
         if self.ADM2.value is None:
             self.ADM2.value = "0"
             self.ADM2.frozen = True
         
-        # here is the similar situation for the mdm
+        # here is the similar situation for the MDM
         if self.MDM.value is None:
             self.MDM.value = "0"
             self.MDM.frozen = True
 
-        # here is the similar situation for the mdm
+        # here is the similar situation for the BDM
         if self.BDM.value is None:
             self.BDM.value = "0"
             self.BDM.frozen = True    
